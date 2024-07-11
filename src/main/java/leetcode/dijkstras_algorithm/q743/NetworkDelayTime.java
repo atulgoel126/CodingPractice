ad9f677281853e730ee7f06f932b67dd83d4df99
Atul Goel <atul.goel.126+github@gmail.com>
2024-07-11 15:16:12 +0530

    Added solution for Q743 - Network Delay Time
    Using Dijktra's algorithm, find the shortest route to each node form starting point k.
    
    Once you have the data distance[] which is 1 based, iterate from index 1 to the end of the array and find the max value in the array.
    If max value is Integer.MAX_VALUE, we can safely assume that the node is unreachable.
    
    return either max value or -1;

--- src/main/java/leetcode/dijkstras_algorithm/q743/NetworkDelayTime.java
+++ src/main/java/leetcode/dijkstras_algorithm/q743/NetworkDelayTime.java
@@ -1,6 +1,60 @@
 package leetcode.dijkstras_algorithm.q743;
+
+import java.util.*;
+
 public class NetworkDelayTime {
-    public int networkDelayTime(int[][] times, int n, int k) {
-        return 0;
+    public int networkDelayTime(int[][] times, int N, int k) {
+        List<Edge>[] graph = new List[N + 1];
+
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int[] edge : times) {
+            int u = edge[0];
+            int v = edge[1];
+            int cost = edge[2];
+            graph[u].add(new Edge(v, cost));
+        }
+
+        PriorityQueue<Edge> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o.cost));
+
+        int[] dist = new int[N + 1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+
+        pq.add(new Edge(k, 0));
+        dist[k] = 0;
+
+        while (!pq.isEmpty()) {
+            Edge current = pq.poll();
+            int u = current.vertex;
+            int d = current.cost;
+
+            if (dist[u] < d) {
+                continue;
+            }
+
+            for (Edge neighbor : graph[u]) {
+                int v = neighbor.vertex;
+                int newDist = dist[u] + neighbor.cost;
+
+                if (newDist < dist[v]) {
+                    dist[v] = newDist;
+                    pq.add(new Edge(v, newDist));
+                }
+            }
+        }
+
+        int maxDist = Arrays.stream(dist).skip(1).max().getAsInt();
+        return maxDist == Integer.MAX_VALUE ? -1 : maxDist;
+    }
+
+    class Edge {
+        int vertex;
+        int cost;
+        public Edge(int vertex, int cost) {
+            this.vertex = vertex;
+            this.cost = cost;
+        }
     }
 }
