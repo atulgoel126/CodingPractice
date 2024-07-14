a1ef2055801399715d7e239b041ff7e66ffaa16d
Atul Goel <atul.goel.126+github@gmail.com>
2024-07-14 22:15:33 +0530

    Added solution for Q2: Adding two numbers as LinkedList
    There were couple of things to be careful about:
    
    - Don't miss the carry over when the main loop breaks.
    - Don't forget to include the last carry when everything is done - example : 99 + 1

--- src/main/java/leetcode/linked_list/q2/AddTwoNumbers.java
+++ src/main/java/leetcode/linked_list/q2/AddTwoNumbers.java
@@ -1,3 +1,52 @@
 package leetcode.linked_list.q2;
+
+import leetcode.commons.ListNode;
+
 public class AddTwoNumbers {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+
+        ListNode dummyHead = new ListNode(-1);
+        int carry = 0;
+        ListNode cur = dummyHead;
+        while (l1 != null && l2 != null) {
+            int sum = l1.val + l2.val + carry;
+            carry = 0;
+            if (sum > 9) {
+                sum = sum - 10;
+                carry = 1;
+            }
+            cur.next = new ListNode(sum);
+            cur = cur.next;
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+
+        while (l1 != null) {
+            int sum = l1.val + carry;
+            cur.next = new ListNode(sum % 10);
+            cur = cur.next;
+            carry = sum / 10;
+            l1 = l1.next;
+        }
+        while (l2 != null) {
+            int sum = l2.val + carry;
+            cur.next = new ListNode(sum % 10);
+            cur = cur.next;
+            carry = sum / 10;
+            l2 = l2.next;
+        }
+
+        if (carry > 0) {
+            cur.next = new ListNode(carry);
+        }
+        return dummyHead.next;
+    }
+
 }
