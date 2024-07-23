0fe69cfef7794f62b147166a70af57e36c6488a2
Atul Goel <atul.goel.126+github@gmail.com>
2024-07-23 19:01:23 +0530

    Added solution for Q48 - rotating a matrix
    Used two simpler approaches - transposing the matrix and then inverting on Y axis.

--- src/main/java/leetcode/matrix/q48/RotateImage.java
+++ src/main/java/leetcode/matrix/q48/RotateImage.java
@@ -1,5 +1,28 @@
 package leetcode.matrix.q48;
 public class RotateImage {
     public void rotate(int[][] matrix) {
+
+        if (matrix == null || matrix.length <= 1|| matrix[0].length <= 1) {
+            return;
+        }
+
+        // invert the matrix where matrix[i][j] -> matrix[j][i]
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j <= i; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // invert the entire rows where rows[0] becomes rows[matrix.length - 1]
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length / 2; j++) {
+                int temp = matrix[i][j];
+                int replacementIndex = matrix.length - 1 - j;
+                matrix[i][j] = matrix[i][replacementIndex];
+                matrix[i][replacementIndex] = temp;
+            }
+        }
     }
 }
--- src/test/java/leetcode/matrix/q48/RotateImageTests.java
+++ src/test/java/leetcode/matrix/q48/RotateImageTests.java
@@ -1,105 +1,87 @@
-//package leetcode.matrix.q48;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import leetcode.matrix.q48.RotateImage;
-//
-//public class RotateImageTests {
-//
-//    private RotateImage ob;
-//
-//    @BeforeEach
-//    public void setUp() {
-//        ob = new RotateImage();
-//    }
-//
-//    @Test
-//    public void testEmptyMatrix() {
-//        int[][] matrix = {};
-//        ob.rotate(matrix);
-//        int[][] expected = {};
-//        assertArrayEquals(expected, matrix);
-//    }
-//
-//    @Test
-//    public void testSingleElementMatrix() {
-//        int[][] matrix = {{1}};
-//        ob.rotate(matrix);
-//        int[][] expected = {{1}};
-//        assertArrayEquals(expected, matrix);
-//    }
-//
-//    @Test
-//    public void testOddSizeMatrix() {
-//        int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-//        ob.rotate(matrix);
-//        int[][] expected = {{7, 4, 1}, {8, 5, 2}, {9, 6, 3}};
-//        assertArrayEquals(expected, matrix);
-//    }
-//
-//    @Test
-//    public void testEvenSizeMatrix() {
-//        int[][] matrix = {{1, 2}, {3, 4}};
-//        ob.rotate(matrix);
-//        int[][] expected = {{3, 1}, {4, 2}};
-//        assertArrayEquals(expected, matrix);
-//    }
-//
-//    @Test
-//    public void testLargeSizeMatrix() {
-//        int[][] matrix = {{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {11, 12, 13, 14, 15}, {16, 17, 18, 19, 20}, {21, 22, 23, 24, 25}};
-//        ob.rotate(matrix);
-//        int[][] expected = {{21, 16, 11, 6, 1}, {22, 17, 12, 7, 2}, {23, 18, 13, 8, 3}, {24, 19, 14, 9, 4}, {25, 20, 15, 10, 5}};
-//        assertArrayEquals(expected, matrix);
-//    }
-//
-//    @Test
-//    public void testMatrixWithDuplicateElements() {
-//        int[][] matrix = {{1, 1, 3}, {4, 5, 6}, {7, 8, 9}};
-//        ob.rotate(matrix);
-//        int[][] expected = {{7, 4, 1}, {8, 5, 1}, {9, 6, 3}};
-//        assertArrayEquals(expected, matrix);
-//    }
-//
-//    @Test
-//    public void testMatrixWithNegativeElements() {
-//        int[][] matrix = {{-1, -2, -3}, {-4, -5, -6}, {-7, -8, -9}};
-//        ob.rotate(matrix);
-//        int[][] expected = {{-7, -4, -1}, {-8, -5, -2}, {-9, -6, -3}};
-//        assertArrayEquals(expected, matrix);
-//    }
-//
-//    @Test
-//    public void testMatrixWithZeroElements() {
-//        int[][] matrix = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
-//        ob.rotate(matrix);
-//        int[][] expected = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
-//        assertArrayEquals(expected, matrix);
-//    }
-//
-//    @Test
-//    public void testMatrixWithMixedElements() {
-//        int[][] matrix = {{1, -2, 3}, {4, 0, -6}, {7, 8, 0}};
-//        ob.rotate(matrix);
-//        int[][] expected = {{7, 4, 1}, {8, 0, -2}, {0, -6, 3}};
-//        assertArrayEquals(expected, matrix);
-//    }
-//
-//    @Test
-//    public void testMatrixWithNonSquareDimensions() {
-//        int[][] matrix = {{1, 2, 3}, {4, 5, 6, 7}, {8, 9, 10, 11}, {12, 13, 14, 15, 16}};
-//        assertThrows(IllegalArgumentException.class, () -> {
-//            ob.rotate(matrix);
-//        });
-//    }
-//
-////    @Test
-////    public void testMatrixWithNullElements() {
-////        int[][] matrix = {{1, 2, 3}, {4, 5, null}, {null, 8, 9}};
-////        assertThrows(NullPointerException.class, () -> {
-////            ob.rotate(matrix);
-////        });
-////    }
-//
-//}
+package leetcode.matrix.q48;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class RotateImageTests {
+
+    private RotateImage ob;
+
+    @BeforeEach
+    public void setUp() {
+        ob = new RotateImage();
+    }
+
+    @Test
+    public void testEmptyMatrix() {
+        int[][] matrix = {};
+        ob.rotate(matrix);
+        int[][] expected = {};
+        assertArrayEquals(expected, matrix);
+    }
+
+    @Test
+    public void testSingleElementMatrix() {
+        int[][] matrix = {{1}};
+        ob.rotate(matrix);
+        int[][] expected = {{1}};
+        assertArrayEquals(expected, matrix);
+    }
+
+    @Test
+    public void testOddSizeMatrix() {
+        int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        ob.rotate(matrix);
+        int[][] expected = {{7, 4, 1}, {8, 5, 2}, {9, 6, 3}};
+        assertArrayEquals(expected, matrix);
+    }
+
+    @Test
+    public void testEvenSizeMatrix() {
+        int[][] matrix = {{1, 2}, {3, 4}};
+        ob.rotate(matrix);
+        int[][] expected = {{3, 1}, {4, 2}};
+        assertArrayEquals(expected, matrix);
+    }
+
+    @Test
+    public void testLargeSizeMatrix() {
+        int[][] matrix = {{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {11, 12, 13, 14, 15}, {16, 17, 18, 19, 20}, {21, 22, 23, 24, 25}};
+        ob.rotate(matrix);
+        int[][] expected = {{21, 16, 11, 6, 1}, {22, 17, 12, 7, 2}, {23, 18, 13, 8, 3}, {24, 19, 14, 9, 4}, {25, 20, 15, 10, 5}};
+        assertArrayEquals(expected, matrix);
+    }
+
+    @Test
+    public void testMatrixWithDuplicateElements() {
+        int[][] matrix = {{1, 1, 3}, {4, 5, 6}, {7, 8, 9}};
+        ob.rotate(matrix);
+        int[][] expected = {{7, 4, 1}, {8, 5, 1}, {9, 6, 3}};
+        assertArrayEquals(expected, matrix);
+    }
+
+    @Test
+    public void testMatrixWithNegativeElements() {
+        int[][] matrix = {{-1, -2, -3}, {-4, -5, -6}, {-7, -8, -9}};
+        ob.rotate(matrix);
+        int[][] expected = {{-7, -4, -1}, {-8, -5, -2}, {-9, -6, -3}};
+        assertArrayEquals(expected, matrix);
+    }
+
+    @Test
+    public void testMatrixWithZeroElements() {
+        int[][] matrix = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+        ob.rotate(matrix);
+        int[][] expected = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+        assertArrayEquals(expected, matrix);
+    }
+
+    @Test
+    public void testMatrixWithMixedElements() {
+        int[][] matrix = {{1, -2, 3}, {4, 0, -6}, {7, 8, 0}};
+        ob.rotate(matrix);
+        int[][] expected = {{7, 4, 1}, {8, 0, -2}, {0, -6, 3}};
+        assertArrayEquals(expected, matrix);
+    }
+}
