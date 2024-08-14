7f7591bf82cd16a0366f082ee54d73037107851f
Atul Goel <atul.goel.126+github@gmail.com>
2024-08-14 18:50:01 +0530

    Added solution for Q42 - Trapping rain water
    I initially came up with the brute force approach, but n2 seemed a lot.
    
    Took a hint and the sliding window solution seemed easier and more obvious.

--- src/main/java/leetcode/array_string/q42/TrappingRainWater.java
+++ src/main/java/leetcode/array_string/q42/TrappingRainWater.java
@@ -1,6 +1,34 @@
 package leetcode.array_string.q42;
 public class TrappingRainWater {
     public int trap(int[] height) {
-        return -1;
+        int n = height.length;
+
+        if (n == 0)
+            return 0;
+
+        int waterTrapped = 0;
+
+        int left = 0;
+        int right = n - 1;
+        int leftMax = 0;
+        int rightMax = 0;
+
+        while (left < right) {
+            if (height[left] < height[right]) {
+                if (height[left] > leftMax) {
+                    leftMax = height[left];
+                }
+                waterTrapped = waterTrapped + leftMax - height[left];
+                left++;
+            } else {
+                if (height[right] > rightMax) {
+                    rightMax = height[right];
+                } else {
+                    waterTrapped = waterTrapped + rightMax - height[right];
+                }
+                right--;
+            }
+        }
+        return waterTrapped;
     }
 }
