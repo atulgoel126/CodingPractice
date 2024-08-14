64a3a53940ecfaf490cb7c6f1fc7195e0dbd33c4
Atul Goel <atul.goel.126+github@gmail.com>
2024-08-14 20:45:05 +0530

    Added solution for Q11 - maxArea of water
    A usual question, the interesting part was creating a separate function to calculate the abstracted the actual problem and made is much easier to solve.

--- src/main/java/leetcode/two_pointers/q11/ContainerWithMostWater.java
+++ src/main/java/leetcode/two_pointers/q11/ContainerWithMostWater.java
@@ -1,6 +1,22 @@
 package leetcode.two_pointers.q11;
 public class ContainerWithMostWater {
     public int maxArea(int[] height) {
-        return -1;
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            maxArea = Math.max(maxArea, getArea(left, right, height));
+            if (height[left] > height[right]) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+        return maxArea;
+    }
+
+    private int getArea(int start, int end, int[] height) {
+        return Math.min(height[start], height[end]) * (end - start);
     }
 }
